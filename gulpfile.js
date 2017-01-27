9d6b08122ceb3a5cfbbb6df10e0d4fb5ad9944bf
Silver <silver.liivamagi@gmail.com>
2017-01-27 10:54:00 +0200

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function(){
+  gulp.src('.')
+    .pipe(webserver({
+      host: '0.0.0.0',
+      port: '8080',
+      path: '/',
+      fallback: 'index.html'
+    }));
+});
+
